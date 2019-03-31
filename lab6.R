9c81daff5f1035defe43dc34863315b9aec08770
lightbulbmoment22617 <lightbulbmoment22617@gmail.com>
2019-03-31 12:11:55 +0100

    lab6

---
+++ lab6.R
@@ -0,0 +1,7 @@
+source("lab06code.R")
+TMINallobs <- read.csv(file="TMINallobs.csv",
+                       header = TRUE,
+                       stringsAsFactors = False)
+TMINalltest <- read.csv(file="TMINalltest.csv",
+                        header = TRUE,
+                        stringsAsFactors = False)
